28c08a85cddd9b39379c6848cde7a02be81461c3
JurgisGuscia <j.d.guscia@gmail.com>
2026-02-27 20:05:09 +0200

    Javascriptas sifravimo funkcionalumui.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var dictionary = null;
-const scrambledTextArray = [];
 
-function generateDictionary(){
-    const dictionary = {};
-    for (let i = 32; i <= 126; i++) {
-        dictionary[String.fromCharCode(i)] = i - 32;
-    }
-    return dictionary;
+function generateDictionaries() {
+  const forward = {};
+  const reverse = {};
+
+  for (let i = 32; i <= 126; i++) {
+    const char = String.fromCharCode(i);
+    const value = i - 32;   // 0..94
+
+    forward[char] = value;
+    reverse[value] = char;
+  }
+
+  return { forward, reverse };
 }
 
-function parce(key){
+function parse(key){
     return key.split("");
 }
 
@@ -22,17 +28,29 @@ function createArrayCounter(array) {
     };
 }
 
+function getKeyByValue(value) {
+  return Object.keys(dictionary).find(key => dictionary[key] === value);
+}
+
 function convertLetter(letter, factor){
-    
+    let newLetterNumber = dictionary[letter] + dictionary[factor];
+
+    newLetterNumber = newLetterNumber % 95;
+    return getKeyByValue(newLetterNumber);
 }
 
 function scramble(messageText, codeKey){
-    !dictionary && (dictionary = generateDictionary());
-    const key = parce(codeKey);
-    const text = parce(messageText);
+    if(!dictionary){
+        const { dictionary, reverseDictionary } = generateDictionaries();
+    }
+    scrambledTextArray.length = 0;
+    const key = parse(codeKey);
+    const text = parse(messageText);
     const keyCounter = createArrayCounter(key);
     text.forEach(letter => {
         let keyFactor = keyCounter();
-        scrambledTextArray.push(convertLetter(letter, dictionary.keyFactor));
+        scrambledTextArray.push(convertLetter(letter, keyFactor));     
     });
+    const finalText = scrambledTextArray.join("");
+    return finalText;
 }
